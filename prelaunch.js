17c778d268871a008ededf0b67984b4e630818ad
lam.elton@gmail.com <lam.elton@gmail.com>
2024-01-11 13:59:20 +0800

    Fixing mobile footer.

--- prelaunch.js
+++ prelaunch.js
@@ -12,100 +12,104 @@
 
     var sectionHeight = totalScrollHeight + (screenHeight /2);     
     if ($(window).width() < 768) {
-    $('.mobile-bottom').css('top', sectionHeight + 'px');
-        
-    $('#first-grid .top-left .fourth').appendTo('.mobile-bottom');
-    $('#second-grid .top-right .cell-image .fourth').appendTo('.mobile-bottom');      
+        $('.mobile-bottom').css('top', sectionHeight + 'px');
+            
+        $('#first-grid .top-left .fourth').appendTo('.mobile-bottom');
+        $('#second-grid .top-right .cell-image .fourth').appendTo('.mobile-bottom');      
 
-    $('#first-grid .bottom-left .fifth').appendTo('.mobile-bottom');
-    $('#first-grid .top-left .fifth').appendTo('.mobile-bottom');
+        $('#first-grid .bottom-left .fifth').appendTo('.mobile-bottom');
+        $('#first-grid .top-left .fifth').appendTo('.mobile-bottom');
 
-    $('#second-grid .top-right .fifth').appendTo('.mobile-bottom');
-    $('#second-grid .bottom-right .fifth').appendTo('.mobile-bottom');
+        $('#second-grid .top-right .fifth').appendTo('.mobile-bottom');
+        $('#second-grid .bottom-right .fifth').appendTo('.mobile-bottom');
 
-    var serene = $('#first-grid .top-left .third');
-    replaceClass(serene, 'third', 'fourth');
-    serene.appendTo('#second-grid .top-right .cell-image');
+        $('.div-block-20').appendTo('.mobile-bottom .fourth');
 
-    $('.div-block-11.cell-image').remove('.first');
-    var img = $('#first-grid .top-left .second');
-    replaceClass(img, 'second', 'first')
-    img.appendTo('.div-block-11.cell-image');
+        var serene = $('#first-grid .top-left .third');
+        replaceClass(serene, 'third', 'fourth');
+        serene.appendTo('#second-grid .top-right .cell-image');
 
-    $('#second-grid .bottom-right .first').remove();
-    $('#second-grid .top-right .third').remove();
+        $('.div-block-11.cell-image').remove('.first');
+        var img = $('#first-grid .top-left .second');
+        replaceClass(img, 'second', 'first')
+        img.appendTo('.div-block-11.cell-image');
 
-    var carousel = $('#second-grid .bottom-right .third');
-    replaceClass(carousel, 'third', 'fifth')
+        $('#second-grid .bottom-right .first').remove();
+        $('#second-grid .top-right .third').remove();
 
-    var spaces = $('#second-grid .bottom-right .second');
-    replaceClass(spaces, 'second', 'third');
+        var carousel = $('#second-grid .bottom-right .third');
+        replaceClass(carousel, 'third', 'fifth')
 
-    img = $('#second-grid .top-right .second');
-    replaceClass(img, 'second', 'first');
-    img.appendTo('#second-grid .bottom-right .slides-container');
+        var spaces = $('#second-grid .bottom-right .second');
+        replaceClass(spaces, 'second', 'third');
 
-    var mobileBottomHeight = $('.mobile-bottom').height();
-    sectionHeight += mobileBottomHeight;
+        img = $('#second-grid .top-right .second');
+        replaceClass(img, 'second', 'first');
+        img.appendTo('#second-grid .bottom-right .slides-container');        
+
+        var mobileBottomHeight = $('.mobile-bottom').outerHeight(true);
+        sectionHeight += mobileBottomHeight;
     }
 
-    $('.section-2').css('height', sectionHeight + 'px');     
+    setTimeout(function() {
+        $('.section-2').css('height', sectionHeight + 'px');     
+    }, 300);    
 
     $(document).ready(function() {
-    scrollScene();
+        scrollScene();
     });
 
     $(window).scroll(function() {
-    scrollScene();
+        scrollScene();
     });
 
     function replaceClass(element, source, target) {
-    element.removeClass(source);
-    element.addClass(target);
+        element.removeClass(source);
+        element.addClass(target);
     }
 
     function scrollScene() {
-    var top = $(window).scrollTop();
-    log(top);
+        var top = $(window).scrollTop();
+        log(top);
 
-    var sceneIndex = 0;
-    for (var i = 0; i < numScenes; i++) {
-        var start = (screenHeight * i) + (screenHeight / 2);
-        var end = start + (screenHeight / 2);
-        log(top + " " + start + " " + end);
-        if (top >= start && top <= end) {
-        changedScene = true;
-        sceneIndex = i + 1;
-        break;
-        } else if (top >= 0 && top < screenHeight / 2) {
-        sceneIndex = 0;
-        break;
-        } else {
-        changedScene = false;
+        var sceneIndex = 0;
+        for (var i = 0; i < numScenes; i++) {
+            var start = (screenHeight * i) + (screenHeight / 2);
+            var end = start + (screenHeight / 2);
+            log(top + " " + start + " " + end);
+            if (top >= start && top <= end) {
+            changedScene = true;
+            sceneIndex = i + 1;
+            break;
+            } else if (top >= 0 && top < screenHeight / 2) {
+            sceneIndex = 0;
+            break;
+            } else {
+            changedScene = false;
+            }
         }
-    }
 
-    changeScene(sceneIndex, changedScene);
+        changeScene(sceneIndex, changedScene);
     }
 
     function changeScene(scene, changedScene) {
-    if (!changedScene) return;
+        if (!changedScene) return;
 
-    scene += 1;
-    scene = scene > numScenes ? numScenes : scene;
-    log(scene);
+        scene += 1;
+        scene = scene > numScenes ? numScenes : scene;
+        log(scene);
 
-    if (prevScene) {
-        $('#main-grid').removeClass('scene-' + prevScene);
-    }
+        if (prevScene) {
+            $('#main-grid').removeClass('scene-' + prevScene);
+        }
 
-    $('#main-grid').addClass('scene-' + scene);
-    prevScene = scene;
+        $('#main-grid').addClass('scene-' + scene);
+        prevScene = scene;
     }
 
     function log(msg) {
-    if (!debug) return;
+        if (!debug) return;
 
-    console.log(msg);
+        console.log(msg);
     }
 })(jQuery)
